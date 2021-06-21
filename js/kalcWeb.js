8471ebce5f7bfec4f2ee7cd2edd188579fed78a0
Bonfim Justino do Nascimento <bonfim1999@gmail.com>
2021-06-21 16:46:25 -0300

    - Refatoração das operações  - Correção de operações com ponto.

--- js/kalcWeb.js
+++ js/kalcWeb.js
@@ -1,168 +1,109 @@
 
 // Tela
 const screen = document.getElementsByClassName('screen')[0];
-const operadores = document.getElementsByClassName('operators')[0].children;
 
 // Operações
-const somar = document.getElementById('somar');
-const subtrair = document.getElementById('subtrair');
-const multiplicar = document.getElementById('multiplicar');
-const dividir = document.getElementById('dividir');
-const limpar = document.getElementById('limpar');
-const igual = document.getElementById('igual');
+const operadores = document.querySelectorAll('.buttons .operators button');
+
+const operadoresValues = Array.from(operadores).map(elem => elem.dataset.val);
 
 // Numeros
-const zero = document.getElementById('zero');
-const um = document.getElementById('um');
-const dois = document.getElementById('dois');
-const tres = document.getElementById('tres');
-const quatro = document.getElementById('quatro');
-const cinco = document.getElementById('cinco');
-const seis = document.getElementById('seis');
-const sete = document.getElementById('sete');
-const oito = document.getElementById('oito');
-const nove = document.getElementById('nove');
-const ponto = document.getElementById('ponto');
+const botoes = document.querySelectorAll('.buttons .numbers button');
 
 // Variavéis auxiliares
-
-let resultado;
+let permitirPonto = true;
 
 // Funções
-function incrementar(valor) {
+function incrementar(valor) {    
     const valorTela = screen.innerText;
+    const valores = valorTela.split('');
+    const ultimoValor = valores[valores.length - 1];
 
-    if (valorTela) {            
-        screen.innerText += valor;        
+    if (valores.length >= 15) return;
+
+    if (valor === '.') {
+        if (!permitirPonto || operadoresValues.includes(ultimoValor)) {
+            return;
+        }
+        permitirPonto = false;
+    }
+
+    // Se houver erro no console limpa e atribui o valor
+    if (valorTela === 'Infinity' || valorTela === '-Infinity') {
+        screen.innerText = valor;
+        return;
+    }
+
+    // Se o ultimo valor for um operador ou ponto e o valor a ser inserido também for substitui o ultimo valor pelo o atual
+    if (operadoresValues.includes(ultimoValor) && operadoresValues.includes(valor) || ultimoValor === '.' && operadoresValues.includes(valor) ) {        
+        valores[valores.length - 1] = valor;
+        screen.innerText = valores.join('');
+    }
+    
+    else if (valorTela) {        
+        screen.innerText += valor;
+        
     } else {
         screen.innerText = valor;        
     }
 }
 
-function convertNumber(valor) {
-    if (valor.indexOf('.') >= 0) {
-        return parseFloat(valor);
-    }
-    return parseInt(valor);
-}
-
-function processar(operador, val1, val2) {
-    const valor1 = convertNumber(val1);
-    const valor2 = convertNumber(val2);
-    
-    switch (operador) {
-        case '+':
-            return valor1 + valor2;
-        case '-':
-            return valor1 - valor2;
-        case 'x':
-            return valor1 * valor2;
-        case '÷':
-            return valor1 / valor2;
-        default:
-            return null;
-    }
-}
 
 function getResultado(operador) {
     const valor = screen.innerText;
+    const valores = valor.split('');    
+    const ultimoValor = valores[valores.length - 1];
+    let resultado;
     
-    let resultadoClean;
-    
-    const operadoresValues = Array.from(operadores).map(elem => elem.dataset.val);
-    operadoresValues.forEach(op => {
-        if (valor.indexOf(op, 1) >= 0) {            
-            const result = valor.split(op);
-            resultadoClean = result.reduce((prev, current) => processar(op, prev, current));
-            return;
+    // Se o ultimo valor for um operador remove, calcula e mostra o resultado
+    if (operadoresValues.includes(ultimoValor)) {
+        valores.splice(valores.length - 1, 1);
+        resultado = eval(valores.join('')).toString();
+        if (resultado.indexOf('.') >= 0) { // caso o numero seja com ponto seta pra false a flag que permit pontuação
+            permitirPonto = false;
         }
-    });
-
-    if (!resultadoClean) {
-        resultadoClean = valor;
+        
+        
+    } else { // senao mostra o resultado
+        resultado = eval(valor).toString();
+        
+        if (resultado.indexOf('.') >= 0) { // caso o numero seja com ponto seta pra false a flag que permit pontuação
+            permitirPonto = false;
+        }
     }
-    screen.innerText = resultadoClean;
+
+    if (resultado === 'Infinity' || resultado === '-Infinity') {
+        resultado = 'Erro ao dividir 0';
+    }
+    screen.innerText = resultado.slice(0, 15);
 }
 
 
 // Eventos
-limpar.addEventListener('click', function (e) {
-    screen.innerText = '';
-    operacoes = [];
+botoes.forEach((button) => {
+    button.addEventListener("click", () => {
+        const valor = button.getAttribute("data-val");
+        if (valor === "clear") {
+            screen.innerText = '';
+            operacoes = [];
+        } else {
+            incrementar(valor);            
+        }
+    });
+    
 });
 
-zero.addEventListener('click', function (e) {
-    incrementar(e.target.dataset.val);
-});
+operadores.forEach((op) => {
+    if (op.getAttribute("data-val") === "=") {
+        op.addEventListener("click", () => {        
+            getResultado(op.getAttribute("data-val"));
+        });
 
-um.addEventListener('click', function (e) {
-    incrementar(e.target.dataset.val);
-});
-
-dois.addEventListener('click', function (e) {
-    incrementar(e.target.dataset.val);
-});
-
-tres.addEventListener('click', function (e) {
-    incrementar(e.target.dataset.val);
-});
-
-quatro.addEventListener('click', function (e) {
-    incrementar(e.target.dataset.val);
-});
-
-cinco.addEventListener('click', function (e) {
-    incrementar(e.target.dataset.val);
-});
-
-seis.addEventListener('click', function (e) {
-    incrementar(e.target.dataset.val);
-});
-
-sete.addEventListener('click', function (e) {
-    incrementar(e.target.dataset.val);
-});
-
-oito.addEventListener('click', function (e) {
-    incrementar(e.target.dataset.val);
-});
-
-nove.addEventListener('click', function (e) {
-    incrementar(e.target.dataset.val);
-});
-
-ponto.addEventListener('click', function (e) {    
-    incrementar(e.target.dataset.val);
-});
-
-somar.addEventListener('click', function (e) {
-    if (screen.innerText) {
-        getResultado();
-        incrementar(e.target.dataset.val);            
+    } else {
+        op.addEventListener("click", () => {
+            const valorClicado = op.getAttribute("data-val");            
+            incrementar(valorClicado);                    
+            permitirPonto = true; 
+        });
     }
- });
-
-subtrair.addEventListener('click', function (e) {
-    if (screen.innerText) {
-        getResultado();
-        incrementar(e.target.dataset.val);            
-    }
-});
-
-multiplicar.addEventListener('click', function (e) {
-    if (screen.innerText) {
-        getResultado();
-        incrementar(e.target.dataset.val);            
-    }    
-});
-
-dividir.addEventListener('click', function (e) {
-    if (screen.innerText) {
-        getResultado();
-        incrementar(e.target.dataset.val);            
-    }  
-});
-
-igual.addEventListener('click', function (e) {
-    getResultado();
 });
